7449011ad42494583bdbc8101d6d30024b07a952
Franco Catena <francocatena@gmail.com>
2013-07-26 11:43:48 -0300

    Remove unnecessary task in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,12 +43,4 @@ namespace :deploy do
     end
   end
   before 'deploy', 'deploy:check_revision'
-
-  desc 'Creates the synmlink to tmp/pids'
-  task :create_tmp_pids_symlink, roles: :app, except: { no_release: true } do
-    run "mkdir -p #{release_path}/tmp"
-    run "mkdir -p #{shared_path}/tmp/pids"
-    run "ln -nfs #{shared_path}/tmp/pids #{release_path}/tmp/pids"
-  end
-  #after 'deploy:update_code', 'deploy:create_tmp_pids_symlink'
 end
