42a5ea3e8d3e4db99e32e7698d16f123dc2b040c
Franco Catena <francocatena@gmail.com>
2013-07-29 17:01:02 -0300

    Now with smiley =)

--- app/mailers/response_mailer.rb
+++ app/mailers/response_mailer.rb
@@ -6,7 +6,17 @@ class ResponseMailer < ActionMailer::Base
     @ticket = ticket
     @body = body
     @to = to
+    attach_smile_image
 
     mail subject: ticket.subject_with_id, from: tenant.email, to: @to
+  end
+
+
+  private
+
+  def attach_smile_image
+    path = File.join Rails.root, 'vendor', 'assets', 'images', 'face-smile.png'
+
+    attachments.inline['smile.png'] = File.read path
   end
 end
