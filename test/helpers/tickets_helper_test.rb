3989024e0bed559c619bf2cbd62e8944c8aaa950
Franco Catena <francocatena@gmail.com>
2013-07-31 00:19:09 -0300

    Tests added to ticket helper

--- test/helpers/tickets_helper_test.rb
+++ test/helpers/tickets_helper_test.rb
@@ -18,4 +18,30 @@ class TicketsHelperTest < ActionView::TestCase
 
     assert_match /label-error/, ticket_status(ticket)
   end
+
+  test 'ticket class' do
+    ticket = tickets :enhancement
+
+    assert_equal 'warning', ticket_class(ticket)
+
+    ticket.status = 'closed'
+
+    assert_equal 'active', ticket_class(ticket)
+
+    ticket.user = nil
+
+    assert_nil ticket_class(ticket)
+  end
+
+  test 'feedbacks label' do
+    ticket = tickets :enhancement
+
+    assert_match /#{t('navigation.show')}/, feedbacks_label(ticket)
+  end
+
+  test 'feedbacks count' do
+    ticket = tickets :enhancement
+
+    assert_match /#{ticket.feedbacks.count}/, feedbacks_count(ticket)
+  end
 end
