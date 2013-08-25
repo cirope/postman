a47cdf1fe6dddd1b40fcce890e95a5cacd10084f
Franco Catena <francocatena@gmail.com>
2013-08-25 15:05:44 -0300

    Add vendor font to assets path...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,9 @@ Postman::Application.configure do
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   config.assets.precompile += %w( .svg .eot .woff .ttf )
 
+  # Add the fonts path
+  config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
