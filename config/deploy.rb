a89e123146d865c04346ec0c5b50e2c66785258e
Franco Catena <francocatena@gmail.com>
2013-10-31 18:35:07 -0300

    config/deploy.rb cleanup...

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,17 +2,15 @@ set :application, 'postman'
 set :user, 'deployer'
 set :repo_url, 'git://github.com/cirope/postman.git'
 
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-
 set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :deploy_via, :remote_cache
 set :scm, :git
 
-set :rbenv_type, :user
-set :rbenv_ruby, '2.0.0-p247'
-
 set :linked_files, %w{config/app_config.yml}
 set :linked_dirs, %w{log}
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.0.0-p247'
 
 set :keep_releases, 5
 
@@ -21,15 +19,6 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
     end
   end
 
