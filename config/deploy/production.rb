eb5341d4ef49f4b67fea2eecd38acf303d185243
Franco Catena <francocatena@gmail.com>
2014-01-14 20:19:08 -0300

    Capistrano 3.1

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,8 @@
 set :stage, :production
 set :rails_env, 'production'
 
-role :all, %w{librujo.com}
+role :web, %w{deployer@librujo.com}
+role :app, %w{deployer@librujo.com}
+role :db,  %w{deployer@librujo.com}
 
 server 'librujo.com', user: 'deployer', roles: %w{web app db}
